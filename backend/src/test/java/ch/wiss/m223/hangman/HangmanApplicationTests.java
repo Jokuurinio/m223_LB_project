83503a974e50d907240085fb533268fe430456bd
Jokuurinio <fabian.wehrle63@gmail.com>
2024-11-22 11:49:13 +0100

    initial backend commit
    v1 with dummy endpoints and user authentification

---
+++ backend/src/test/java/ch/wiss/m223/hangman/HangmanApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.m223.hangman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- references/backend/m223_rbac_jwt_block4/src/main/java/ch/wiss/m223/wiss_quiz2024/model/ERole.java
+++
@@ -1,7 +0,0 @@
-package ch.wiss.m223.wiss_quiz2024.model;
-
-public enum ERole {
-  ROLE_USER,
-  ROLE_MODERATOR,
-  ROLE_ADMIN
-}
